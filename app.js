876ca23261a5ad129bac1e2568c8974a2ec01305
Shubham Dubey <shubhamshd99@gmail.com>
2023-02-01 13:06:49 +0530

    initial express boilerplate code

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+// db connection start---------------
+// mongoose.set('strictQuery', true);
+const database = process.env.MONGOLAB_URI;
+console.log("database env param", database);
+mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log('db connect'))
+.catch(err => console.log(err));
+// db connection closed**************
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log("Server started at http://localhost:" + PORT))
